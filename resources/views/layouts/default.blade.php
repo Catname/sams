b5d7f0c4b4bba80677f050e517934c4a0a763f48
Zod <tomcath@foxmail.com>
2021-04-19 17:00:45 +0800

    基础页面

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>@yield('title', '学生成绩管理系统')</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
---
+++ resources/views/static_pages/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', '查看学生信息')
+@section('content')
+  <h1>学生</h1>
+@stop
