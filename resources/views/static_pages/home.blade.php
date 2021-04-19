2bb20a517fc7f77b8c376c7732bfae80c16707e3
Zod <tomcath@foxmail.com>
2021-04-19 18:11:51 +0800

    完善

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.default')
 @section('title', '查看学生信息')
 @section('content')
-  <h1>学生</h1>
+  <div class="jumbotron">
+    <p>
+      <a class="btn btn-lg btn-success" href="{{ route('students.create') }}" role="button">添加学生</a>
+    </p>
+  </div>
 @stop
---
+++ resources/views/students/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.default')
+@section('title', '注册')
+
+@section('content')
+  <h1>添加学生</h1>
+@stop
