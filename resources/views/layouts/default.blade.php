96eb32489bbbf1f715c50da60c0d1ff8359b1039
Zod <tomcath@foxmail.com>
2021-04-19 17:24:49 +0800

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <title>@yield('title', '学生成绩管理系统')</title>
-  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
 
