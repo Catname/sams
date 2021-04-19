de5e28e2734473ea5ec3f6b073b41aa1058853c4
Zod <tomcath@foxmail.com>
2021-04-19 23:50:53 +0800

    完善主页信息

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.default')
-@section('title', '查看学生信息')
+@section('title', '主页')
 @section('content')
   <div class="jumbotron">
+    <h1>欢迎来到学生成绩管理系统</h1>
+    <p class="lead">
+      点击 <a href="{{ route('students.index') }}">查看学生列表</a>
+    </p>
+    <p>
+      或者，你可以添加学生信息
+    </p>
     <p>
       <a class="btn btn-lg btn-success" href="{{ route('students.create') }}" role="button">添加学生</a>
     </p>
